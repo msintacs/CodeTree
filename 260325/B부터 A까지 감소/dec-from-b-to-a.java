021af504b09fb0aef722f260a86140b7e8efb9a9
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-03-25 16:29:11 +0900

    Title: B부터 A까지 감소, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260325/B부터 A까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        for (int i=B; i>=A; i--) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
