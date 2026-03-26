4e9558e1fb3a8250faa7f6cd3a99357b16edcde3
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-03-26 09:09:00 +0900

    Title: 윤년인가, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260326/윤년인가/is-leap-year.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int Y = Integer.parseInt(br.readLine());
+
+        if (Y % 4 == 0) {
+            if (Y % 100 == 0 && Y % 400 != 0) {
+                System.out.println(false);
+            } else {
+                System.out.println(true);
+            }
+        }
+    }
+}
