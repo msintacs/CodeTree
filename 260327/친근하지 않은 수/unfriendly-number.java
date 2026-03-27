92a53d80eef03721f9d53244e51a2b304039e7a8
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-03-27 10:43:01 +0900

    Title: 친근하지 않은 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260327/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        for (int i=1; i<=N; i++) {            
+            if (i % 2 == 0) continue;
+            if (i % 3 == 0) continue;
+            if (i % 5 == 0) continue;
+            count++;
+        }
+
+        System.out.println(count);
+    }
+}
