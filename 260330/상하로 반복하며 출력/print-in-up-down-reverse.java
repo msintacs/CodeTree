9ff78c8c1271126547e5820611cbf2409aac5485
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-03-30 15:16:08 +0900

    Title: 상하로 반복하며 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                if (j % 2 == 0) {
+                    System.out.print(i + 1);
+                } else {
+                    System.out.print(N - i);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
