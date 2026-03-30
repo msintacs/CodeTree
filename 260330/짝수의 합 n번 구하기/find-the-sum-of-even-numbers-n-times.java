e85e6a2f757216d00c8461641c30b2db09df03eb
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-03-30 10:39:38 +0900

    Title: 짝수의 합 n번 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int sum = 0;
+            for (int j=a; j<=b; j++) {
+                if (j % 2 == 0) 
+                    sum += j;
+            }
+
+            System.out.println(sum);
+        }
+
+    }
+}
