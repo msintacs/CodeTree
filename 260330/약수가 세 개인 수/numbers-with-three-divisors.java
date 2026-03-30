3d332752921da32069671f0a160cb5db209d14f2
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-03-30 11:08:10 +0900

    Title: 약수가 세 개인 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int totalCount = 0;    
+        for (int i=start; i<=end; i++) {
+            int cnt = 0;            
+            for (int j=1; j<=i; j++)
+                if (i % j == 0) cnt++;
+
+            if (cnt == 3)
+                totalCount++;
+        }
+        System.out.println(totalCount);
+    }
+}
