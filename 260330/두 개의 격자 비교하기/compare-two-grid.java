7b5fd8be85dac5111cbf8dd156381af6ccc0ac2c
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-03-30 13:43:14 +0900

    Title: 두 개의 격자 비교하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N * 2][M];
+        for (int i=0; i<N*2; i++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            for (int j=0; j<M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<M; j++) {
+                System.out.print(arr[i][j] != arr[i + N][j] ? 1 + " " : 0 + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
