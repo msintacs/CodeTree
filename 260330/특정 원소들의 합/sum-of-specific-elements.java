60c03af2b9d5d92f1f764d32cfab01b4087ce9d3
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-03-30 14:46:36 +0900

    Title: 특정 원소들의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260330/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int sum = 0;
+        for (int i=0; i<4; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j=0; j<4; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                if (i >= j) {
+                    sum += tmp;
+                }
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
