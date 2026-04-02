91e1a121c4a2cc56637ce308479bba31d62a6148
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 16:10:06 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i=n; i<=100; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
