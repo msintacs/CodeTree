3fd4fbe7847c8cbb0ef17bada387f455d811183c
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 10:35:31 +0900

    Title: 삼항연산자, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/삼항연산자/ternary-operator.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+        System.out.println(score == 100 ? "pass" : "failure");
+    }
+}
