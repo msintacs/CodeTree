436027c7841278e18f95870db70f289ef19f0711
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 16:51:37 +0900

    Title: N까지 자연수 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/N까지 자연수 출력/print-1-to-n.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 1;
+        while(cnt <= N)
+            System.out.print(cnt++ + " ");
+    }
+}
