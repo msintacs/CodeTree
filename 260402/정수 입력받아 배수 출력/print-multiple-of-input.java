3942b06ce3e5958fdccb00c833f971818890e33f
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 16:26:22 +0900

    Title: 정수 입력받아 배수 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i=N; i<=N*5; i+=N) {
+            System.out.print(i + " ");
+        }
+    }
+}
