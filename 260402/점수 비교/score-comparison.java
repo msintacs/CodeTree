b79f92ffd9ef6955fe7d94a89854f880bf39d1d7
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 13:23:00 +0900

    Title: 점수 비교, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/점수 비교/score-comparison.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int mathA = Integer.parseInt(st.nextToken());
+        int englishA = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int mathB = Integer.parseInt(st.nextToken());
+        int englishB = Integer.parseInt(st.nextToken());
+
+        if (mathA > mathB && englishA > englishB)
+            System.out.print(1);
+        else 
+            System.out.print(0);
+    }
+}
