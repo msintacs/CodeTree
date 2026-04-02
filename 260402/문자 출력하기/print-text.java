c405b2ea23d6b192142c3b767fcba92baf96db23
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 17:38:54 +0900

    Title: 문자 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/문자 출력하기/print-text.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        for (int i=0; i<8; i++) {
+            System.out.print(input);
+        }
+    }
+}
