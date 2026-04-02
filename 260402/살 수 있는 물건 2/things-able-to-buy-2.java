8b97e5197a815a2cb44fa0b47f60317b834c50c6
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 11:11:37 +0900

    Title: 살 수 있는 물건 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N >= 3000)
+            System.out.println("book");
+        else if (N >= 1000)
+            System.out.println("mask");
+        else if (N >= 500)
+            System.out.println("pen");
+        else
+            System.out.println("no");
+    }
+}
