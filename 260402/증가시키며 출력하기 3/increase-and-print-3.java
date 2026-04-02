0ff734fbe5622c9c03531cdcbceeb2eb9bfa0572
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 17:31:23 +0900

    Title: 증가시키며 출력하기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cnt = 10;
+        while (cnt <= 26) {
+            System.out.print(cnt + " ");
+            cnt += 2;
+        }
+    }
+}
