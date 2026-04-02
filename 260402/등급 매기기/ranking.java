037a3b25dc8a578802171438bcb1f9a2c4d97c29
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 11:06:54 +0900

    Title: 등급 매기기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/등급 매기기/ranking.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N >= 90)
+            System.out.println("A");
+        else if (N >= 80)
+            System.out.println("B");
+        else if (N >= 70)
+            System.out.println("C");
+        else if (N >= 60)
+            System.out.println("D");
+        else
+            System.out.println("F");
+    }
+}
