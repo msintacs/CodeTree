0d015ee1e25d13f77b751a0596b2344216c09774
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-02 13:19:03 +0900

    Title: 4번의 크기 비교, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<4; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            System.out.println(A > tmp ? 1 : 0);
+        }
+    }
+}
