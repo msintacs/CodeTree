3348e0b91a0e0b55b48461ffe3eefc00d4b6c471
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 15:55:49 +0900

    Title: 정사각형 두 개 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/정사각형 두 개 출력/output-two-rectangles.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N*2; i++) {
+            for (int j=0; j<N; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            if (i == N - 1)
+                System.out.println();
+        }
+    }
+}
