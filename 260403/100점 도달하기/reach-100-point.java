98b88a4d03933939ea628f1de8c8e849505a36cd
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 09:27:31 +0900

    Title: 100점 도달하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/100점 도달하기/reach-100-point.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+
+        for (int i=score; i<=100; i++) {
+            System.out.print(getGrade(i) + " ");
+        }
+    }
+
+    public static String getGrade(int score) {
+        if (score >= 90) {
+            return "A";
+        } else if (score >= 80) {
+            return "B";
+        } else if (score >= 70) {
+            return "C";
+        } else if (score >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
