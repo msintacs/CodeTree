8a4e346204ba5b9397f1216af79dcc0820d6f942
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 13:57:20 +0900

    Title: 20대의 평균 나이, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260403/20대의 평균 나이/average-age-of-20.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int sum = 0;
+        int cnt = 0;
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n >= 30) 
+                break;
+
+            sum += n;
+            cnt++;
+        }
+
+
+        System.out.printf("%.2f", (double) sum / cnt);
+    }
+}
