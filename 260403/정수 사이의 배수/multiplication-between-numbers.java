a7fec4aebfea7118977d0f8b1c056e2ac1634624
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 11:31:21 +0900

    Title: 정수 사이의 배수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/정수 사이의 배수/multiplication-between-numbers.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+        int cnt = 0;
+        for (int i=A; i<=B; i++) {
+            if (i % 5 == 0 || i % 7 == 0) {
+                sum += i;
+                cnt++;
+            }
+        }
+
+        System.out.printf("%d %.1f", sum, (double) sum / cnt);
+    }
+}
