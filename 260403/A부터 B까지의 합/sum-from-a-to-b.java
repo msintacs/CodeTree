19a24b77145e8d02f1bc35c2bffffe946e60064b
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 10:31:12 +0900

    Title: A부터 B까지의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/A부터 B까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int sum = 0;
+        for (int i=A; i<=B; i++) {
+            sum += i;
+        }  
+
+        System.out.println(sum);
+    }
+}
