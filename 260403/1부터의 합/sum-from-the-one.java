10ae480019776fb45139b6d6f7604535d69ef2f2
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 13:40:23 +0900

    Title: 1부터의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i=1; i<=N; i++) {
+            sum += i;
+            if (sum >= N) {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
