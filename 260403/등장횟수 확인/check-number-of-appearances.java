4c3240b7c5ae246a2a84d655b69b1aa33d5c422c
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 10:27:53 +0900

    Title: 등장횟수 확인, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        for (int i=0; i<5; i++) {
+            if (Integer.parseInt(br.readLine()) % 2 == 0)
+                cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
