c22c432e069bf2cc4bc5d8760c9fce176edeb538
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 13:11:48 +0900

    Title: 완전수 판별기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/완전수 판별기/perfect-number-discriminator.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i=1; i<N; i++) {
+            if (N % i == 0)
+                sum += i;
+        }
+
+        if (sum == N) {
+            System.out.print("P");
+        } else {
+            System.out.print("N");
+        }
+    }
+}
