71824de4c5a055a8f61fe532b58e6021e5831290
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 15:39:02 +0900

    Title: 합성수 판별기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=2; i<N; i++) {
+            if (N % i == 0) {
+                System.out.print("C");
+                return;
+            }
+        }
+
+        System.out.print("N");
+    }
+}
