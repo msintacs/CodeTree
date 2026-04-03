7543d0a703736cde399291c55625926c67c7699e
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 10:24:45 +0900

    Title: 윤년 횟수 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        for (int i=1; i<=N; i++) {
+            if (i % 4 == 0) {
+                if (i % 100 == 0 && i % 400 != 0) {
+                    ;
+                } else {
+                    cnt++;
+                }
+            } 
+        }
+
+        System.out.print(cnt);
+    }
+}
