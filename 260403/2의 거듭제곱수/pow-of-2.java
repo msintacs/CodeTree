39890ac641e24c635d3352b164863f01968f846e
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 14:41:10 +0900

    Title: 2의 거듭제곱수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/2의 거듭제곱수/pow-of-2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        while (N != 1) {
+            N /= 2;
+            cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
