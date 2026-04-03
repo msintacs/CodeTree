69f0967425ef18684aa22c934fc685405ef38b6f
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-03 16:04:47 +0900

    Title: 별표 출력하기 5, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/별표 출력하기 5/print-star-5.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N-i; j++) {
+                for (int k=0; k<N-i; k++) {
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
