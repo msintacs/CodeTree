93ec02121a654777e13d1b5ec778741b08204901
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-06 09:26:11 +0900

    Title: 별표 출력하기 10, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260406/별표 출력하기 10/print-star-10.java
+++ 260406/별표 출력하기 10/print-star-10.java
@@ -8,41 +8,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int up = 1;
-        int down = N;
-        for (int i=0; i<N; i++) {
+        for (int i=0; i<N*2; i++) {
             if (i % 2 == 0) {
-                for (int j=0; j<up; j++) {
+                for (int j=0; j<i/2+1; j++) {
+                    System.out.print("* ");
+                }                
+            } else {
+                for (int j=0; j<N-(i-1)/2; j++) {
                     System.out.print("* ");
                 }
-
-                up++;
-            } else {
-                for (int j=0; j<down; j++) {
-                    System.out.print("* ");                    
-                }
-
-                down--;
             }
-
-            System.out.println();
-        }
-
-        for (int i=0; i<N; i++) {
-            if (i % 2 == 0) {
-                for (int j=0; j<down; j++) {
-                    System.out.print("* ");
-                }
-
-                down--;
-            } else {
-                for (int j=0; j<up; j++) {
-                    System.out.print("* ");                    
-                }
-
-                up++;
-            }
-
             System.out.println();
         }
     }
