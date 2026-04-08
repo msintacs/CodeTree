1d598f9c5e5fe603277228234402de5c842c7861
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-08 13:12:59 +0900

    Title: 모양대로 수 출력하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                cnt += 2;
+                if (cnt == 10) {
+                    cnt = 2;
+                }
+                System.out.print(cnt + " ");
+            }
+            System.out.println();
+        }
+    }
+}
