e47b7ab5c4745d431b2b90154a036aa228e9088c
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-08 12:35:26 +0900

    Title: 정사각형 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/정사각형 출력/print-square.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 1;
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                System.out.print(cnt + " ");
+                cnt++;
+            }
+            System.out.println();
+        }
+    }
+}
