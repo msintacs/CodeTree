ffdc8e1ab7b5a04b51be0f04e3814095ce229b17
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-08 17:35:31 +0900

    Title: 숫자 피라미드 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<i+1; j++) {
+                cnt++;
+                System.out.print(cnt + " ");
+            }
+            System.out.println();
+        }
+    }
+}
