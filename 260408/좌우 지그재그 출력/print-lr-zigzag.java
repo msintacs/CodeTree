d6dbcca3a419fbb5c584d233082bd8bb8ff348bd
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-08 13:26:02 +0900

    Title: 좌우 지그재그 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/좌우 지그재그 출력/print-lr-zigzag.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+        for (int i=0; i<N; i++) {
+            if (i % 2 == 0) {
+                for (int j=0; j<N; j++) {                    
+                    cnt++;
+                    System.out.print(cnt + " ");
+                }
+            } else {
+                cnt = cnt + N;
+                for (int j=0; j<N; j++) {
+                    System.out.print((cnt-j) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
