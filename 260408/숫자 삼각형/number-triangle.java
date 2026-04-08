f36715128a75ec70cdd73d97cfd9c0915ece0de9
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-08 15:23:54 +0900

    Title: 숫자 삼각형, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260408/숫자 삼각형/number-triangle.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<=i; j++) {
+                System.out.print((j+1) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
