82d9d0693a7e87e6279546c8b0c010f59bf97974
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-15 16:09:23 +0900

    Title: 특정 수 도달하기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260415/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+        int cnt = 0;
+        while (true) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if (value >= 250) break;
+
+            sum += value;
+            cnt++;
+        }
+
+        System.out.printf("%d %.1f", sum, (double) sum / cnt);
+    }
+}
