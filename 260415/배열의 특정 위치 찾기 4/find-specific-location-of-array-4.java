c9fbd8eeca5f9e29cfa7b8f45a25f443fe357a51
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-15 16:32:49 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260415/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+        int cnt = 0;
+        while (st.hasMoreTokens()) {
+            int n = Integer.parseInt(st.nextToken());
+            if (n == 0) break;
+
+            if (n % 2 == 0) {
+                sum += n;
+                cnt++;
+            }
+        }
+
+        System.out.print(cnt + " " + sum);
+    }
+}
