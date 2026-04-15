f55549d951242acd858d35d1d3a5c9e7f9a02771
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-15 14:33:22 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260415/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 1;
+        for (int i=0; i<N; i++) {
+            
+            if (cnt > 9) {
+                cnt = 1;
+            }
+
+            for (int j=0; j<i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j=0; j<N-i; j++) {
+                System.out.print(cnt++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
