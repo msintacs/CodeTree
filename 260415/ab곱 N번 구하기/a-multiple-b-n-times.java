ddf778ea675546a702b4d675299d296fb6648697
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-15 15:32:51 +0900

    Title: ab곱 N번 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260415/ab곱 N번 구하기/a-multiple-b-n-times.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int mul = 1;
+            for (int j=a; j<=b; j++) {
+                mul *= j;
+            }
+            System.out.println(mul);
+        }
+    }
+}
