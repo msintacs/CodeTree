e424fc6a442e63cea9784cd43d28a88a7578a463
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-15 15:11:22 +0900

    Title: 특이한 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260415/특이한 출력/special-print.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                System.out.print("(" + (i+1) + ", " + (j+1) + ") ");
+                if (((i+1) + (j+1)) % 4 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
