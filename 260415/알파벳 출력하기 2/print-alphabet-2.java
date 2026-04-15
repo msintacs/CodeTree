fe717336383a62306f888d3191beb23714eae26d
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-15 15:29:58 +0900

    Title: 알파벳 출력하기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260415/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        char c = 'A';
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N; j++) {
+                if (j < i) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(c++ + " ");
+                }
+
+                if (c == '[') c = 'A';
+            }
+            System.out.println();
+        }
+    }
+}
