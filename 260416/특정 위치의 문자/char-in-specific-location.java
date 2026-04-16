59bdafd266566fd9ea5929cba046fc850873e26b
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-16 10:31:21 +0900

    Title: 특정 위치의 문자, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260416/특정 위치의 문자/char-in-specific-location.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));        
+        String str = "LEBROS";
+        int idx = str.indexOf(br.readLine());
+        System.out.print(idx == -1 ? "None" : idx);
+    }
+}
