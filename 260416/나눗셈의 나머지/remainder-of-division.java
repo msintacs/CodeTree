c3e2193e777b4b11ce880d966070ac5b297406cc
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-16 10:22:51 +0900

    Title: 나눗셈의 나머지, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260416/나눗셈의 나머지/remainder-of-division.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] count = new int[10];
+        
+        int dividend  = Integer.parseInt(st.nextToken());
+        int divisor = Integer.parseInt(st.nextToken());
+
+        while (dividend > 0) {
+            count[dividend % divisor]++;
+            dividend /= divisor;
+        }
+
+        int sum = 0;
+        for (int i=0; i<count.length; i++) {
+            sum += count[i] * count[i];
+        }
+
+        System.out.print(sum);
+    }
+}
