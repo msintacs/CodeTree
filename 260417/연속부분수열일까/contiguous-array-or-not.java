78cb22f3e4e7bb61e9f1e3987570df4e7b575573
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-17 11:26:46 +0900

    Title: 연속부분수열일까, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260417/연속부분수열일까/contiguous-array-or-not.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N1 = Integer.parseInt(st.nextToken());
+        int N2 = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N1];
+        int[] B = new int[N2];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N1; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N2; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0;
+        int end = B.length;
+        boolean isConsecutive = true;
+        while (end < A.length) {
+
+            isConsecutive = true;
+
+            int idx = 0;
+            for (int i=start; i<end; i++) {
+                if (A[i] != B[idx]) {                    
+                    isConsecutive = false;
+                    break;
+                }
+
+                idx++;
+            }
+
+            if (isConsecutive) {
+                System.out.print("Yes");
+                break;
+            }
+
+            start++;
+            end++;            
+        }
+
+        if (!isConsecutive) System.out.print("No");
+    }
+}
