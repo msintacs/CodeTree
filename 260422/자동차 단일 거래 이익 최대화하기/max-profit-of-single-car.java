606af1f963ae13066b970a8ea735d4dc39353653
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-22 15:23:44 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260422/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int max = Integer.MIN_VALUE;
+        int[] arr = new int[N];
+
+        for (int i=0; i<N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        for (int i=0; i<arr.length; i++) {
+            for (int j=i+1; j<arr.length; j++) {
+                if (arr[j] - arr[i] > max)
+                    max = arr[j] - arr[i];
+            }
+        }
+        System.out.print(max > 0 ? max : 0);
+    }
+}
