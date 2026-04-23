e3b45f6889e5e978c5b440ed776f630c8a0f673b
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-23 14:17:37 +0900

    Title: 지그재그로 정수 채우기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260423/지그재그로 정수 채우기/zigzag-numbering.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<M; j++) {
+                if (j % 2 == 0) {
+                    System.out.print((j * N + i) + " ");
+                } else {
+                    System.out.print((j * N + (N - 1 -i)) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
