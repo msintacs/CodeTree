bd8ee669f625aaa491593b3d0ff50a7c9680b40a
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-23 15:33:11 +0900

    Title: 배열로 사각형 만들기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260423/배열로 사각형 만들기/print-array-in-rectangle.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int[][] arr = new int[5][5];
+
+        for (int i=0; i<5; i++) {
+            arr[0][i] = 1;
+            arr[i][0] = 1;
+        }
+
+        for (int i=1; i<5; i++) {
+            for (int j=1; j<5; j++) {
+                arr[i][j] = arr[i-1][j] + arr[i][j-1];
+            }
+        }
+
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<5; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
