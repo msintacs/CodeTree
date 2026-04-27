2244a9919d58c2f03dca2906f388c2e9bba18453
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-27 16:01:53 +0900

    Title: 문자열 전부 붙이기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/문자열 전부 붙이기/paste-all-string.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<N; i++) {
+            sb.append(br.readLine());
+        }
+
+        System.out.print(sb.toString());
+    }
+}
