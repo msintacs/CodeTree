24086f43a748019a1f7d5b8fbd6b22959e6f9783
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-27 16:23:01 +0900

    Title: 부분문자열 위치 구하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260427/부분문자열 위치 구하기/find-location-of-substring.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String target = br.readLine();
+
+        int answer = -1;
+
+        for (int i=0; i<str.length() - 1; i++) {
+            String tmp = str.charAt(i) + "" + str.charAt(i+1);
+            if (target.equals(tmp)) {
+                answer = i;
+                break;
+            }
+        }
+
+        System.out.print(answer);
+    }
+}
