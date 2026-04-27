aabea6a94acbf90b0f03747fe042ed8a9e4e666c
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-27 10:39:02 +0900

    Title: 문자열 두번 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.print(str + "\n" + str);
+    }
+}
