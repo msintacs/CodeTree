6d76d112e376a945ce4825632d1511a2dec10d07
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-27 16:44:44 +0900

    Title: a로 채워넣기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<str.length(); i++) {
+            if (i == 1 || i == str.length() - 2) {
+                sb.append("a");
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
