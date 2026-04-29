26937d1fe8a99f4e0d2f57781d0239f1a5ce674c
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-29 13:53:37 +0900

    Title: 그 전 알파벳, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/그 전 알파벳/before-the-alphabet.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
+
+        int nRes = c - 1;
+        if (nRes < 97) {
+            nRes = 122;
+        }
+
+        System.out.print((char) nRes);
+    }
+}
