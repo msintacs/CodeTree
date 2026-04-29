0ebaa9fe8531e5b90e88443869c991e16189b72a
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-29 13:45:42 +0900

    Title: 아스키코드표 맞추기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i=0; i<5; i++) {
+            System.out.print((char) Integer.parseInt(st.nextToken()) + " ");
+        }
+    }
+}
