075df45888fa3f1acb5a0053b0acfebbf0f5c2f1
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-29 14:12:35 +0900

    Title: 대문자 소문자 바꾸기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (Character.isLowerCase(c)) {
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
