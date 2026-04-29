df6d180763de539fb957ea131b0456ee0949ebfe
SEONGBONG <121651839+msintacs@users.noreply.github.com>
2026-04-29 14:14:31 +0900

    Title: 붙여서 합하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/붙여서 합하기/add-and-add.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = st.nextToken();
+        String B = st.nextToken();
+
+        int nA = Integer.parseInt(A+B);
+        int nB = Integer.parseInt(B+A);
+
+        System.out.print(nA + nB);
+    }
+}
